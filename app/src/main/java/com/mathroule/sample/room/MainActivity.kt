95ce5b7bfffa087389138945ee0a64bade22bed6
Mathieu Rul <mathroule@gmail.com>
2020-06-28 12:56:11 +0200

    Close database explicitly

--- app/src/main/java/com/mathroule/sample/room/MainActivity.kt
+++ app/src/main/java/com/mathroule/sample/room/MainActivity.kt
@@ -2,9 +2,9 @@ package com.mathroule.sample.room
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mathroule.sample.room.database.SampleDatabase
 import com.mathroule.sample.room.database.entity.Book
 import com.mathroule.sample.room.database.entity.User
-import com.mathroule.sample.room.database.SampleDatabase
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,12 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sampleDao = SampleDatabase.getDatabase(this).sampleDao()
+        val database = SampleDatabase.getDatabase(this)
+
+        val sampleDao = database.sampleDao()
 
         val user = User(name = "John Smith")
         val userId = sampleDao.insert(user)
 
         val book = Book(userId = userId, title = "Android for dummies")
         sampleDao.insert(book)
+
+        database.close()
     }
 }
